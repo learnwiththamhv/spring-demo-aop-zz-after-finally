3e7848d3031136784be7a6592e6cc267c35f29bc
thamhv <learnwiththamhv@gmail.com>
2019-07-20 21:33:58 +0700

    first commit

---
+++ src/com/luv2code/aopdemo/DemoConfig.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.luv2code.aopdemo")
+public class DemoConfig {
+
+	@Bean
+	AccountDAO accountDAO() {
+		return new AccountDAO();
+	}
+
+	@Bean
+	MembershipDAO membershipDAO() {
+		return new MembershipDAO();
+	}
+}
